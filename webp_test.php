3b84c146b98b9065f86ea623e92863595222a400
rainkide@gmail.com <rainkide@gmail.com>
2013-06-14 16:06:07 +0800

    update

--- webp_test.php
+++
@@ -1,6 +0,0 @@
-<?php
-$opts = getopt('f:');
-$blob = file_get_contents("/home/lvbenwei/".$opts['f']);
-$res = image2webp($blob);
-$arr = explode('/',$opts['f']);
-file_put_contents("/home/lvbenwei/".$arr[count($arr)-1].".webp",$res);
